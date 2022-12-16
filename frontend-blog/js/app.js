988f2ae81c2a19bfb5294c6dd86d9dcd84b63252
Eman_Elsayed <112521958+EmanElsayed2002@users.noreply.github.com>
2022-12-16 22:21:13 +0200

    Add files via upload

---
+++ frontend-blog/js/app.js
@@ -0,0 +1,25 @@
+function clicklinkedin() {
+    window.open("https://www.linkedin.com/in/eman-elsayed-a67318218/", "_blank");
+}
+function clickgithub() {
+    window.open("https://github.com/EmanElsayed2002", "_blank");
+}
+function clicktelegram() {
+    window.open("https://t.me/emy2192002", "_blank");
+}
+function clickemail() {
+    window.open("mailto:emy2192002@gmail.com", "_blank");
+}
+function clickfacebook() {
+    window.open("https://www.facebook.com/profile.php?id=100048783972642", "_blank");
+}
+function clicktwitter() {
+    window.open("https://twitter.com/emanelsayed2002", "_blank");
+}
+function web() {
+    window.location.href = "frontend.html";
+}
+function back_end() {
+    window.location.href = 'backend.html';
+}
+
